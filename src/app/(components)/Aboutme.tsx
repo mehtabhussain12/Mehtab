c05880e9348394c5561517f5d69439288d7b10e1
Mehtab Hussain <156640664+mehtabhussain12@users.noreply.github.com>
2025-01-06 18:07:19 +0500

    Update Aboutme.tsx

--- src/app/(components)/Aboutme.tsx
+++ src/app/(components)/Aboutme.tsx
@@ -8,8 +8,7 @@ const Aboutme = () => {
         <div className="flex flex-col justify-center items-center text-white">
           <h3 className="text-4xl font-semibold">About me</h3>
           <p className=" text-center text-lg sm:leading-loose sm:max-w-4xl mx-4 mt-4 text-white sm:text-md   leading-6">
-            I am a final-year Software Engineering student at Government Boys
-            Polytechnic Institute with expertise in Front-End React Web
+            I am a UI/UX designer  with expertise in Front-End React Web
             Development. I am well-equipped to create dynamic and responsive
             user interfaces, adapting to industry trends effectively.
           </p>
